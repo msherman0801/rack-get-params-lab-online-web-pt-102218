50b03045531844e6d222241ecc56320e4ed35c3b
Matthew Sherman <msherman0801@gmail.com>
2019-01-29 01:16:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
         resp.write "We don't have that item"
       else
         @@cart << param
-        @@cart.delete!(param)
+        @@cart.delete(p)
         resp.write "added #{param}"
       end
     end
